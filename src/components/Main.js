fc79bbc483d7293d6c0656ea43f73c199603ed6c
Johnnyr81 <emailjohn81@gmail.com>
2020-11-03 14:59:36 -0600

    input and video

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 
 function Main(props) {
 
+    
+
     return (
+
+        <div>
+
+            <div>Youtube Search</div>
+            
+            <input className="search" type="search" name="search" id="search" placeholder="Search" />
+
+            <YouTube videoId="2g811Eo7K8U"/>
         
-        <div>Youtube Search</div>
+        </div>
 
     )
 
