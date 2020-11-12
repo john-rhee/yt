d6dfcb79ac087be0ffe8f80b8e6a49c6104461b1
Johnnyr81 <emailjohn81@gmail.com>
2020-11-11 21:30:24 -0600

    add button

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,29 @@ import YouTube from 'react-youtube';
 
 function Main(props) {
 
-    
+    const handleChanges = e => {
+
+      };
+
+    const submitForm = e => {
+        e.preventDefault();
+        console.log("Button Working")
+
+        }
 
     return (
 
         <div>
 
             <div>Youtube Search</div>
+
+            <form onSubmit={submitForm} >
             
             <input className="search" type="search" name="search" id="search" placeholder="Search" />
+
+            <button type="submit">Search</button>
+
+            </form>
 
             <YouTube videoId="2g811Eo7K8U"/>
         
