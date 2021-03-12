a7d98af07ed1981c6977c007db7d9d0a70b96cd5
Johnnyr81 <emailjohn81@gmail.com>
2021-03-12 09:00:30 -0600

    change api

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -17,25 +17,38 @@ export const searched_words = (sWords) => dispatch => {
 
 export const getResults = (q) => dispatch => {
 
-    
-
     axios
-        .get("https://youtube-search-results.p.rapidapi.com/youtube-search/?q=" + q, {
+        .get(`https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&type=video&part=snippet&maxResults=10&q=` + q, {
 
-            headers: {
-                "x-rapidapi-key": "",
-                "x-rapidapi-host": "youtube-search-results.p.rapidapi.com",
-                "useQueryString": true
-            }
           })
         .then(res => {
             console.log('response',res)
-            dispatch({type:GET_VIDEO,payload:res.data.items[1].id})
+            console.log('response',res.data.items[1].id.videoId)
+            dispatch({type:GET_VIDEO,payload:res.data.items[1].id.videoId})
             
         })
         .catch(error => {
             
             // dispatch({type:GET_FAILURE,payload:error})
     })
+
+    // axios
+    //     .get("https://youtube-search-results.p.rapidapi.com/youtube-search/?q=" + q, {
+
+    //         headers: {
+    //             "x-rapidapi-key": "86adcb47a2msheafa1ea838cc811p183211jsn624c5d1a6362",
+    //             "x-rapidapi-host": "youtube-search-results.p.rapidapi.com",
+    //             "useQueryString": true
+    //         }
+    //       })
+    //     .then(res => {
+    //         console.log('response',res)
+    //         dispatch({type:GET_VIDEO,payload:res.data.items[1].id})
+            
+    //     })
+    //     .catch(error => {
+            
+    //         // dispatch({type:GET_FAILURE,payload:error})
+    // })
     
 };
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import {reducer} from './reducers/Reducer'
 import * as serviceWorker from './serviceWorker';
+require('dotenv').config()
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)));
